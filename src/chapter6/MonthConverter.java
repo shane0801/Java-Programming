9697284287107382aa4ef6a96503496d81ea7359
shane0801 <roshane0801@gmail.com>
2024-03-04 14:01:23 +0200

    Initial commit

---
+++ src/chapter6/MonthConverter.java
@@ -0,0 +1,9 @@
+package chapter6;
+
+public class MonthConverter {
+
+    public static void main(String args[]){
+        System.out.println(Month.getMonth(1));
+        System.out.println(Month.getMonth("January"));
+    }
+}
